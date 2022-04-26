be2930069c625016663006a5c5768eb7329a087a
User <User@USER-PC>
2022-04-27 01:09:18 +0400

    Add project files.

---
+++ ArrayManager/Program.cs
@@ -0,0 +1,38 @@
+﻿{
+    ArrayManager array = new();
+    array.GetMax(array.Create(7));
+    Console.ReadLine();
+}
+class ArrayManager
+{
+    #region Create
+    public int[] Create(int lenght)
+    {
+        int[] array = new int[lenght];
+        Random random = new();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(10, 100);
+            Console.Write(array[i] + " ");
+        }
+        return array;
+    }
+    #endregion
+
+    #region Getmax
+    public int GetMax(int[]array)
+    {
+        int max = array[0];
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine(max);
+        return max;
+    }
+    #endregion
+}
